mikeizbicki/fac

commit 8a84eba5465354b74afd3ef4e914ff23bcdeeb47

make builds much faster by caching git info

fac/BuildSystem.py
@@ -252,12 +252,13 @@ class BuildSystem:
         # if a file is dirty or not in the repo,
         # we use the last modified time;
         # this should only happen if auto_commit=False
-        dirty_files = [item.a_path for item in self.repo.index.diff(None)]
-        staged_files = [item.a_path for item in self.repo.index.diff('HEAD')]
-        if (path in dirty_files or
-            path in staged_files or
-            path in self.repo.untracked_files
-            ):
+        # computing whether a file is dirty is expensive,
+        # so we precompute a set once and store it for future calls of this function
+        if not hasattr(self, '_dirty_files'):
+            modified_files = [item.a_path for item in self.repo.index.diff(None)]
+            staged_files = [item.a_path for item in self.repo.index.diff('HEAD')]
+            self._dirty_files = set(modified_files) | set(staged_files) | set(self.repo.untracked_files)
+        if path in self._dirty_files:
             mtime = os.path.getmtime(path)
             return mtime
 

commit 510fb8270897fcc71c23af3b4694cb1f9663cd31

fix bug due to out of date cache introduced in 8a84eba5465354b74afd3ef4e914ff23bcdeeb47; still should be fast

fac/BuildSystem.py
@@ -257,8 +257,8 @@ class BuildSystem:
         if not hasattr(self, '_dirty_files'):
             modified_files = [item.a_path for item in self.repo.index.diff(None)]
             staged_files = [item.a_path for item in self.repo.index.diff('HEAD')]
-            self._dirty_files = set(modified_files) | set(staged_files) | set(self.repo.untracked_files)
-        if path in self._dirty_files:
+            self._dirty_files = set(modified_files) | set(staged_files)
+        if path in self._dirty_files or self._is_path_untracked(path):
             mtime = os.path.getmtime(path)
             return mtime
 
@@ -276,6 +276,18 @@ class BuildSystem:
         # the above code should handle all possible cases,
         # so this should never happen
         raise ValueError(f'path={path}')
+
+    def _is_path_untracked(self, path):
+        '''
+        Helper function for _committed_date that returns whether a file is tracked by git or not in O(1) time.
+        '''
+        try:
+            # If file is tracked, this won't raise an exception
+            self.repo.git.ls_files('--error-unmatch', path)
+            return False
+        except:
+            # File is untracked if it exists but ls-files fails
+            return os.path.exists(path)
 
     ########################################
     # methods for building