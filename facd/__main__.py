b777c5b1c6e757c67d2b9b6d2606c8de869792ea
Mike Izbicki <mike@izbicki.me>
2026-03-20 14:12:07 -0700

    change log formats

--- facd/__main__.py
+++ facd/__main__.py
@@ -92,7 +92,8 @@ class BroadcastHandler(logging.Handler):
 # Attach handler to your build system logger
 #logger = logging.getLogger("fac")  # adjust to match your logger name
 handler = BroadcastHandler()
-handler.setFormatter(logging.Formatter('%(asctime)s - %(levelname)s - %(message)s'))
+#handler.setFormatter(logging.Formatter('%(asctime)s - %(levelname)s - %(message)s'))
+handler.setFormatter(logging.Formatter('%(message)s'))
 logger.addHandler(handler)
 
 async def log_generator():
