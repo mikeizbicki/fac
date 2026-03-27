f07f30240a84a06e129f9a6dad1a9218c69e66bb
Mike Izbicki <mike@izbicki.me>
2026-03-26 21:15:38 -0700

    [geni] Add thaw function to undo freeze transformations

--- fac/util/freeze.py
+++ fac/util/freeze.py
@@ -43,4 +43,58 @@ def freeze(obj):
     return obj
 
 
+def thaw(obj):
+    """Recursively convert frozendicts to dicts and frozensets to lists.
 
+    >>> thaw(frozendict({'a': 1, 'b': 2}))
+    {'a': 1, 'b': 2}
+
+    >>> thaw(frozendict({'nested': frozendict({'x': 1})}))
+    {'nested': {'x': 1}}
+
+    >>> sorted(thaw(frozenset({1, 2, 3})))
+    [1, 2, 3]
+
+    >>> thaw('hello')
+    'hello'
+
+    >>> thaw(42)
+    42
+
+    >>> thaw(None)
+
+    >>> thaw(frozenset())
+    []
+
+    >>> thaw(frozendict({}))
+    {}
+
+    >>> thaw(freeze({'a': 1, 'b': 2})) == {'a': 1, 'b': 2}
+    True
+
+    >>> thaw(freeze({'nested': {'x': 1}})) == {'nested': {'x': 1}}
+    True
+
+    >>> sorted(thaw(freeze([1, 2, 3]))) == sorted([1, 2, 3])
+    True
+
+    >>> thaw(freeze('hello')) == 'hello'
+    True
+
+    >>> thaw(freeze(42)) == 42
+    True
+
+    >>> thaw(freeze(None)) is None
+    True
+
+    >>> thaw(freeze([])) == []
+    True
+
+    >>> thaw(freeze({})) == {}
+    True
+    """
+    if isinstance(obj, (dict, frozendict)):
+        return {k: thaw(v) for k, v in obj.items()}
+    if isinstance(obj, (list, set, frozenset)):
+        return [thaw(item) for item in obj]
+    return obj
