e5880709c694a14ede8b32da18f708aba51d87bd
Mike Izbicki <mike@izbicki.me>
2026-05-13 11:46:23 -0700

    possibly fix bug for github actions

--- facd/__main__.py
+++ facd/__main__.py
@@ -59,11 +59,11 @@ async def lifespan(app: FastAPI):
 app = FastAPI(title="fac build server", lifespan=lifespan)
 
 # prepare /static mount point
-static_path = files("facd") / "static"
+static_path = str(files("facd") / "static")
 app.mount("/static", StaticFiles(directory=static_path), name="static")
 
 # prepare templates
-templates_path = files("facd") / "static"
+templates_path = str(files("facd") / "static")
 templates = Jinja2Templates(directory=str(templates_path))
 
 
